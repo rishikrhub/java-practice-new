88561a52e2a4400da7bc1403245b160caf890cba
rishikrhub <119436665+rishikrhub@users.noreply.github.com>
2024-04-26 15:44:03 +0000

    java

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(String[] args) {
+    //int randomNum = (int)(Math.random() * 56);  // 0 to 100
+    //System.out.println(randomNum);
+//System.out.println(Math.sqrt(200));
+//System.out.println(Math.max(100,200));
+//int x = 46;
+//int y = 50;
+//System.out.println(Math.max(x,y));
+
