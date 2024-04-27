9b9c91606d3af1a32ddf425027e9d9a6996fe36b
rishikrhub <119436665+rishikrhub@users.noreply.github.com>
2024-04-27 14:22:29 +0000

    my

--- Main.java
+++ Main.java
@@ -1,10 +1,17 @@
 public class Main {
   public static void main(String[] args) {
-    //int randomNum = (int)(Math.random() * 56);  // 0 to 100
-    //System.out.println(randomNum);
-//System.out.println(Math.sqrt(200));
-//System.out.println(Math.max(100,200));
-//int x = 46;
-//int y = 50;
-//System.out.println(Math.max(x,y));
+    //System.out.println("Hello World");
 
+    int Age = 1;
+    if (Age>18) {
+      System.out.println("You can drive a car.");
+    } else{
+      System.out.println("You can't drive a car.");
+    }
+
+    int time = 20;
+    String result;
+    result = (time < 18) ? "Good day." : "Good evening.";
+    System.out.println(result);
+  }
+}
