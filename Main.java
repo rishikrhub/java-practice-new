42da25c0218750b67d96eb2e45d9ae8085fc2e3a
rishikrhub <119436665+rishikrhub@users.noreply.github.com>
2024-05-01 14:55:36 +0000

    newwwwww

--- Main.java
+++ Main.java
@@ -1,34 +1,29 @@
-paaaaaaaaaaaaaaaaaaaa
+
 import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    //System.out.println("Hello World");
-
-    //int Age = 1;
-    //if (Age>18) {
-      //System.out.println("You can drive a car.");
-    //} else{
-      //System.out.println("You can't drive a car.");
-    //}
-
-    //int time = 20;
-    //String result;
-    //result = (time < 18) ? "Good day." : "Good evening.";
-    //System.out.println(result);
-
     int age;
     System.out.println("Enter Your age");
-    Scanner sc = new Scanner(System.in);
-    age = sc.next.Int();
-    
-
-    if (age>58){
-      System.out.println("You are Experienced Employ");
+    try (Scanner scanner = new Scanner(System.in)) {
+      age = scanner.nextInt();
     }
-    else if(age>42){
-      System.out.println("You are Semi Experienced Employ");
+    if (age>52){
+      System.out.println("You are Experienced ");
     }
-    
+    else if (age>41){
+      System.out.println("You are Semi Experienced");
+    }
+    else if (age>34){
+      System.out.println("You are in Progression");
+    }
+    else{
+      System.out.println("You are Not Experienced");
+    }
+    if (age>2){
+      System.out.println("You are not a Baby");
+    }
   }
 }
+
+
